a2052f5eab39533258d17cfa71f96b8efabee652
kali21se <kali21se@gmail.com>
2024-06-09 23:54:40 +0300

    Add try-with-resources for executors

--- src/main/java/task7/Main.java
+++ src/main/java/task7/Main.java
@@ -9,24 +9,25 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ExecutorService executorService = Executors.newFixedThreadPool(10);
-        ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
-        scheduledExecutorService.scheduleAtFixedRate(() -> {
-            var buyerCount = random.nextInt(0, 2);
-            var goodsCount = random.nextInt(1, 4);
-            for (int i = 0; i < buyerCount; i++) {
-                var buyer = new Buyer();
-                executorService.execute(() -> {
-                    buyer.enterToMarket();
-                    buyer.takeBasket();
-                    for (int j = 0; j < goodsCount; j++) {
-                        buyer.chooseGoods();
-                        buyer.putGoodsToBasket();
-                    }
-                    buyer.goOut();
-                    System.out.println("Shopping is done for " + buyer);
-                });
-            }
-        }, 0, 1, TimeUnit.SECONDS);
+        try (ExecutorService executorService = Executors.newFixedThreadPool(10);
+             ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor()) {
+            scheduledExecutorService.scheduleAtFixedRate(() -> {
+                var buyerCount = random.nextInt(0, 2);
+                var goodsCount = random.nextInt(1, 4);
+                for (int i = 0; i < buyerCount; i++) {
+                    var buyer = new Buyer();
+                    executorService.execute(() -> {
+                        buyer.enterToMarket();
+                        buyer.takeBasket();
+                        for (int j = 0; j < goodsCount; j++) {
+                            buyer.chooseGoods();
+                            buyer.putGoodsToBasket();
+                        }
+                        buyer.goOut();
+                        System.out.println("Shopping is done for " + buyer);
+                    });
+                }
+            }, 0, 1, TimeUnit.SECONDS);
+        }
     }
 }
